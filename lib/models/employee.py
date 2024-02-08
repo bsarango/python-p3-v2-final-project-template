c5f0c527dc8cf0a4b1c61a1d0b5316f8367438b6
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2024-02-07 20:06:51 -0500

    first_name property getter and finder methods created

--- lib/models/employee.py
+++ lib/models/employee.py
@@ -4,11 +4,23 @@ class Employee:
 
     all = {}
 
-    def __init__(self,first_name, last_name, job_title, department, id = None):
+    def __init__(self, first_name, last_name, job_title, department, id = None):
         self.first_name = first_name
         self.last_name = last_name
         self.job_title = job_title
         self.department = department
 
 
+    @property
+    def first_name(self):
+        return self._first_name
+
+    @first_name.setter
+    def first_name(self,first_name):
+        if isinstance(first_name,str) and len(first_name)>0:
+            self._first_name = first_name
+        else:
+            raise ValueError("Last name must be a non-empty string")
+
+    
 
