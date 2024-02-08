bbfb672a1f01e7741594b40374846a245fa708ed
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2024-02-07 19:46:56 -0500

    Employee class created in employee.py, __init__ method created with attributes

--- lib/models/employee.py
+++ lib/models/employee.py
@@ -1,1 +1,14 @@
 from models.__init__ import CURSOR, CONN
+
+class Employee():
+
+    all = {}
+
+    def __init__(self,first_name, last_name, job_title, department, id = None):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.job_title = job_title
+        self.department = department
+
+
+
