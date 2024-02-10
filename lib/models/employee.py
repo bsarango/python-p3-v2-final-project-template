9d14e21121c2eddf126142edbe4fd254f449b90f
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2024-02-10 14:28:15 -0500

    job_title and department property getters and setters created in employee class

--- lib/models/employee.py
+++ lib/models/employee.py
@@ -20,7 +20,39 @@ class Employee:
         if isinstance(first_name,str) and len(first_name)>0:
             self._first_name = first_name
         else:
-            raise ValueError("Last name must be a non-empty string")
+            raise ValueError("First name must be word and cannot be blank!")
 
-    
+    @property
+    def last_name(self):
+        return self._last_name
+
+    @last_name.setter
+    def last_name(self):
+        if isinstance(last_name,str) and len(last_name)>0:
+            self._last_name
+        else:
+            raise ValueError("Last name must be a word and cannot be blank!")
+
+    @property
+    def job_title(self):
+        return self._job_title
+
+    @job_title.setter
+    def job_title(self,job_title):
+        if isinstance(job_title,str) and len(job_title)>0:
+            self._job_title = job_title
+        else:
+            raise ValueError("The job title must be word(s) and cannot be blank!")
+
+    @property
+    def department(self):
+        return self._department
+
+    @department.setter
+    def department(self,department):
+        if isinstance(department,str) and len(department):
+            self._department = department
+        else:
+            raise ValueError("The department has to be a word(s) and cannot be blank!")
+
 
