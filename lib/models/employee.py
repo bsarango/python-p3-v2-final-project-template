4e0c232cc3f7a366b02e0b27b2aa66a04ec0f86e
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2024-02-16 14:20:47 -0500

    find_by_id class method created in Employee

--- lib/models/employee.py
+++ lib/models/employee.py
@@ -140,3 +140,15 @@ class Employee:
         rows = CURSOR.execute(sql).fetchall()
         
         return [cls.instance_from_db(row) for row in rows]
+
+    @classmethod
+    def find_by_id(cls,id):
+        sql = """
+            SELECT *
+            FROM departments
+            WHERE id = ?
+        """
+
+        row = CURSOR.execute(sql,(id,)).fetchone()
+
+        return cls.instance_from_db(row) if row else None
