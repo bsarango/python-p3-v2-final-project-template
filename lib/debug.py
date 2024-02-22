7e982ad02e988f4381a096dd2d62bb66d1577423
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2024-02-22 14:29:28 -0500

    reset_database() function created in debug.py; drop and create database methods called; seed data entered

--- lib/debug.py
+++ lib/debug.py
@@ -7,4 +7,20 @@ from models.order import Order
 import ipdb
 
 
+def reset_database():
+    Employee.drop_table
+    Employee.create_table
+    Order.drop_table
+    Order.create_table
+
+    unit_clerk = Employee.create("Jane", "Doe", "Unit Clerk", "Neurology")
+    telemetry_technician = Employee.create("Brian", "Sarango", "Telemetry Technician", "Telemetry")
+    neuro_sdu_rn = Employee.create("Amy", "Jones", "Nurse", "Neurology")
+
+    Order.create("Remote Telemetry Placement","Smith",0,telemetry_technician.id)
+    Order.create("Administer Medication","Lee",1,neuro_sdu_rn.id)
+    Order.create("Transfer Patient","Jones",0,unit_clerk.id)
+    Order.create("Diet","Lee",0,neuro_sdu_rn.id)
+    Order.create("Remote Telemetry Placement", Jones, telemetry_technician.id)
+
 ipdb.set_trace()
