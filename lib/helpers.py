760bd98164986178c58570613462cf54405aae84
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2024-02-25 21:12:42 -0500

    list_employees() helper method updated in helpers.py

--- lib/helpers.py
+++ lib/helpers.py
@@ -3,9 +3,9 @@ from models.employee import Employee
 from models.order import Order
 
 def list_employees():
-    employees = Employee.get_all()
-    for employee in employees:
-        print()
+    # employees = Employee.get_all()
+    for count, ele in enumerate(employees):
+        print(count,": ",ele[first_name], " ",ele[last_name])
     # print("Performing useful function#1.")
 
 def department_menu():
