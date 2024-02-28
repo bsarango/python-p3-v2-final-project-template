ffe895157c9a1ab95e862d6bdca8699cce6cca62
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2024-02-28 18:37:00 -0500

    employee_submenu() function created in helpers.py; employees_menu() updated -input added for employee selction, employee_submenu called

--- lib/helpers.py
+++ lib/helpers.py
@@ -15,7 +15,12 @@ def employees_menu():
     employees = Employee.get_all()
     print("Select one of the employees by choosing the number next to their name to view their details.")
     list_employees(employees)
-    print
+    employee_index = input("Select one of the employees by selecting the number next to them: ")
+    employee_submenu(employees[employee_index-1])
+
+def employee_submenu(employee):
+    print(f"You are currently viewing {employee[first_name]} {employee[last_name]}'s profile. Do you wish to perform any actions?")
+
 
 def exit_program():
     print("Goodbye!")
