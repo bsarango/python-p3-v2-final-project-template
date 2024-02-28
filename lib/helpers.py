e844459c076c54cfe48bb2b5bdd97bcbcf017acb
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2024-02-28 18:22:11 -0500

    employees_menu functino updated, list_employees() called to list employees. employees variable passed into list_employees; list_employees arguments updated to have an argument for list of employees

--- lib/helpers.py
+++ lib/helpers.py
@@ -2,7 +2,7 @@
 from models.employee import Employee
 from models.order import Order
 
-def list_employees():
+def list_employees(employees):
     # employees = Employee.get_all()
     for count, ele in enumerate(employees):
         print(count,": ",ele[first_name], " ",ele[last_name])
@@ -10,6 +10,12 @@ def list_employees():
 
 def department_menu():
     pass
+
+def employees_menu():
+    employees = Employee.get_all()
+    print("Select one of the employees by choosing the number next to their name to view their details.")
+    list_employees(employees)
+    print
 
 def exit_program():
     print("Goodbye!")
