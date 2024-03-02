8669d337792aa11faeaedfec0b5b76c4fbf9d4f4
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2024-03-01 22:38:56 -0500

    try and except statements removed from delete_employee() in helper.py

--- lib/helpers.py
+++ lib/helpers.py
@@ -109,16 +109,12 @@ def delete_employee(employee):
     print("Are you sure you want to delete this employee? Enter 'y' for yes or 'n' for no")
     choice = input(">")
     if choice == "y" or choice == "Y":
-        try:
-            employee.delete()
-            print(f"{employee.first_name} {employee.last_name} has been removed from the system.")
-            return
-        
-        except Exception as exec:
-            print("There was en error removing this employee. Try again.", exec)
+        employee.delete()
+        print(f"{employee.first_name} {employee.last_name} has been removed from the system.")
+        return
     
     else:
-        print("That isn't a valid choice. Please choose again!")
+        print("There was an issue removing this employee!")
     
 def employees_menu():
     while True:
